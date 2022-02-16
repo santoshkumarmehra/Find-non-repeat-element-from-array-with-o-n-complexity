6979d8e70f906ee0486e007230f90c8d7d09fd60
Santosh Kumar Mehra <61575110+santoshkumarmehra@users.noreply.github.com>
2022-02-16 23:21:12 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-str=["santosh","nobita","ban10","kick","himalayan","awesome"]
-max=0
-index=0
-for i in range(0,len(str)):
-	if len(str[i])>max:
-		max=len(str[i])
-		index=i
-print("\n","max length of character = ",str[index])
-print("\n","max character in single string = ",index)
